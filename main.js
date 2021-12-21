58f7264fe43588243e89de2905b978e449096ea2
moalaminn <moalaminn@gmail.com>
2021-12-21 15:35:09 +0000

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+class snakePart {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 let speed = 7;
 
 let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
+const snakeParts = [];
+let tailLength = 2;
+
+let appleX = 5;
+let appleY = 5;
 
 let xVel = 0;
 let yVel = 0;
@@ -14,6 +26,10 @@ let yVel = 0;
 function drawGame() {
     clearScreen();
     changeSnakePos();
+    
+
+    checkAppleCollision();
+    drawApple();
     drawSnake();
     setTimeout(drawGame, 1000/speed);
 }
@@ -24,13 +40,39 @@ function clearScreen() {
 }
 
 function drawSnake() {
+
+    ctx.fillStyle='green';
+    for(let i = 0; i < snakeParts.length; i++) {
+        let part = snakeParts[i];
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+
+    snakeParts.push(new snakePart(headX, headY));
+    if(snakeParts.length > tailLength) {
+        snakeParts.shift();
+    }
+
     ctx.fillStyle = 'orange';
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
 }
 
 function changeSnakePos() {
     headX = headX + xVel;
     headY = headY + yVel;
+}
+
+function drawApple() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize);
+}
+
+function checkAppleCollision() {
+    if (appleX === headX && appleY === headY) {
+        appleX = Math.floor(Math.random() * tileCount);
+        appleY = Math.floor(Math.random() * tileCount);
+        tailLength++;
+    }
 }
 
 document.addEventListener('keydown', keyDown);
